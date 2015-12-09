857095f5cefeac68b408015efc4c99540f070a19
Jonny Adshead <jonny.adshead@gmail.com>
2015-12-09 16:48:37 +0000

    slight update to cla interface spacing

--- cla/tic_tac_toe/application.rb
+++ cla/tic_tac_toe/application.rb
@@ -10,7 +10,7 @@ module TicTacToe
     def start_app
       UI.output
       UI.output 'Welcome to TicTacToe'
-      UI.output 'Quick Game?'
+      UI.output 'Quick Game? (player vs computer : 3x3 board'
       settings unless UI.receive =~ /^y/i
       loop do 
         play 
@@ -21,13 +21,11 @@ module TicTacToe
         UI.output 'Change settings?'
         settings if UI.receive =~ /^y/i
       end
-
       UI.output
       UI.output 'Thank you for playing.'
     end
 
     private
-
     def settings
       choose_player
       choose_board_size
@@ -41,7 +39,6 @@ module TicTacToe
       UI.output
       UI.output 'Player 1'
       @player_one = player_type(UI.receive)
-      UI.output
       UI.output 'Player 2'
       @player_two = player_type(UI.receive)
       UI.output
@@ -53,7 +50,6 @@ module TicTacToe
       UI.output '  3: 3x3'
       UI.output '  4: 4x4'
       UI.output '  N: NxN'
-      UI.output
       size = UI.receive
       @board_size = (size.to_i >= 1) ? size : 3
       UI.output
@@ -75,12 +71,14 @@ module TicTacToe
       @board_size ||= 3
     end
 
-
     def player_type(player)
       if player =~ /^p/i
         HumanPlayer
       elsif player =~ /^c/i
         ComputerPlayer
+      else
+        UI.output ' Invalid player type, please try again.'
+        choose_player
       end
     end
 
